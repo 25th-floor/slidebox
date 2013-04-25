ffa5507e659f758dc2d969cc1dfd472f4671b136
Jan Michael Auer <jan.auer@me.com>
2013-04-25 17:57:22 +0200

    Code rewrite and minor fixes.

--- slidebox.js
+++ slidebox.js
@@ -1,51 +1,63 @@
 /**
  * slidebox - Simple Zepto plugin which turns your checkboxes into iPhone like slideboxes
- * version: 0.1
- * dependecies:
- * - zepto.js framework http://zeptojs.com
- * 
+ * Modified by 25th floor
+ *
  * Copyright(c) 2011 Michal Kuklis <michal.kuklis@gmail.com>
  * MIT Licensed
+ *
+ * @version 0.2
+ * @author Michal Kuklis <michal.kuklis@gmail.com>
+ * @author Thomas Subera <ts@25th-floor.com>
+ * @author Jan Michael Auer <ja@25th-floor.com>
+ *
+ * @see zepto.js framework http://zeptojs.com
  */
 
-(function($) {
-  $.fn.slidebox = function (options) {
-    var TMPL = '<label class="slidebox"><span></span></label>';
-    // default settings
-    var settings = {
-      change: function (el, onOff, key, value) {}, // change callback
-      speed: 0.2 // toggle speed
-    };
-    
-    settings = $.extend(settings, options);
-    // toggle element
-    var toggle = function (e) {
-      var label = $(this)
-        , checkbox = label.prev()
-        , checked = checkbox.attr('checked');
-      
-      label.anim({backgroundPosition:'100%'}, settings.speed)
-        .toggleClass('onoff');
+(function ($) {
 
-      if (checked) {
-        checkbox.removeAttr('checked'); 
-      }
-      else {
-        checkbox.attr('checked', true); 
-      }
-      // execute callback in the context of the checkbox
-      settings.change.call(checkbox, checkbox, !checked);
-    }
-    
-    this.each(function (el) {
-      var checkbox = $(this)
-        , box = $(TMPL);
-      checkbox.after(box);
-      box.bind('tap').bind('click', toggle).bind('swipe', toggle);
-      if (!checkbox.attr('checked')) {
-        box.addClass('onoff'); 
-      }
-      checkbox.hide();
-    }); 
-  }
+	$.fn.slidebox = function (options) {
+		var TMPL = '<label class="slidebox"><span></span></label>';
+		// default settings
+		var settings = {
+			change : function (el, onOff, key, value) { }, // change callback
+			speed  : 0.2 // toggle speed
+		};
+
+		settings = $.extend(settings, options);
+
+		// toggle element
+		function toggle (check) {
+			var label = $(this),
+				checkbox = label.prev(),
+				checked = typeof check == 'boolean' ? !check : checkbox.attr('checked');
+
+			label.toggleClass('onoff', checked);
+
+			if (checked) {
+				checkbox.removeAttr('checked');
+			} else {
+				checkbox.attr('checked', true);
+			}
+
+			// execute callback in the context of the checkbox
+			settings.change.call(checkbox, checkbox, !checked);
+		}
+
+		this.each(function (el) {
+			var checkbox = $(this),
+				box = $(TMPL);
+
+			checkbox.after(box);
+			box.on('click', toggle)
+				.on('swipeLeft', function () { toggle.call(this, false); })
+				.on('swipeRight', function () { toggle.call(this, true); });
+
+			if (!checkbox.attr('checked')) {
+				box.addClass('onoff');
+			}
+			checkbox.hide();
+		});
+	}
+
 })(Zepto);
+
